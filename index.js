30018d6ef71873d5e0b808416e31e56316c8aeeb
Ankit Sharma <asharma@dainsta.com>
2023-09-13 21:47:33 +0530

    Tried fix

--- index.js
+++ index.js
@@ -1,33 +1,35 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors =require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv").config();
 const app = express();
 
-const stripeRoute =  require('./routes/stripeRoute');
+const stripeRoute = require("./routes/stripeRoute");
 
-app.use(cors());
-app.use('/stripe', stripeRoute);
-app.use(express.json());
-app.use(bodyParser.json())
+app.use((req, res, next) => {
+  if (req.originalUrl === "/stripe/handle-payment-success") {
+    next();
+  } else {
+    express.json()(req, res, next);
+    app.use(cors());
+    app.use(express.json());
+    app.use(bodyParser.json());
+  }
+});
 
-const connectToMongo = require("./db")
+app.use("/stripe", stripeRoute);
+
+const connectToMongo = require("./db");
 connectToMongo();
 
-const commanRoute = require('./routes/commonRoute')
-const authRoute = require('./routes/auth');
-const userRoute = require('./routes/userRoute');
+const commanRoute = require("./routes/commonRoute");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/userRoute");
 
 let port = process.env.PORT;
 
+app.use("/api/auth", authRoute);
+app.use("/comman", commanRoute);
+app.use("/", userRoute);
 
-
-app.use('/api/auth',authRoute);
-app.use('/comman', commanRoute);
-app.use('/', userRoute);
-
-
-
-
-app.listen(port, ()=>
- console.log(`Server listening at port ${port}`));
+app.listen(port, () => console.log(`Server listening at port ${port}`));
