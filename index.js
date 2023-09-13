79645227c417be2bd3a693279f890d4d06c8702f
Ankit Sharma <asharma@dainsta.com>
2023-09-13 21:51:22 +0530

    Added cors for all route

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ require("dotenv").config();
 const app = express();
 
 const stripeRoute = require("./routes/stripeRoute");
-
+app.use(cors());
 app.use((req, res, next) => {
   if (req.originalUrl === "/stripe/handle-payment-success") {
     console.log('came here for app suer stripe')
     next();
   } else {
     express.json()(req, res, next);
-    app.use(cors());
+    
     app.use(express.json());
     app.use(bodyParser.json());
   }
